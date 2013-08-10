be0095c6115b1d35d84d9515277782e941104fd6
Vince Masci <vincentmasci@iinet.net.au>
2013-08-10 10:54:37 +1000

    Initialize repository

---
+++ rails/fapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fapp::Application.config.secret_key_base = '110e7be23b29d29064eece9bad6b5a6a083f476e087bf2026cc767a16534e169b3dc1e7b1888b911eb9d56c02003d88e1c3dd0d9f65a5055b14e91d801988991'
