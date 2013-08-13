2975a6eb507b6fda92208597854aaba0da9cd6e4
Vince Masci <vincentmasci@iinet.net.au>
2013-08-14 07:23:08 +1000

    Initial Commit

---
+++ pinterest/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinterest::Application.config.secret_key_base = 'ec91904688f420a4bbc6ebb0e86e7fe8c618f27bd91f37acb423155e3a86228b7a81ca57e7767b219f47140782d4d325c34531ef0aa6a20cc617b6715799785b'
--- pinterest/config/routes.rb
+++ pinterest/config/routes.rb
@@ -1,8 +1,4 @@
-Fapp::Application.routes.draw do
-  resources :microposts
-
-  resources :users
-
+Pinterest::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
